5afb2d48ad5e62691a1d50f5e70fd760c82bd075
JoaBarreraCalix <joansebastianbusiness@gmail.com>
2024-09-15 16:21:44 -0500

    Se arregla error en carpetas por uso de mayusculas, moviendo todo a minuscula acrode con lo sugerido por sonarlint

--- src/test/java/com/example/emazon/infrastructure/in/rest/BrandRestControllerTest.java
+++ src/test/java/com/example/emazon/infrastructure/in/rest/BrandRestControllerTest.java
@@ -23,7 +23,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 
 import java.util.Arrays;
-import java.util.List;
 
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
